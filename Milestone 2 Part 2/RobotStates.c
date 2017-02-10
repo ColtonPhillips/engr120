d24593ae93b16a3d675b76334382b9cc504fd27b
Colton Phillips <coltonjphillips@gmail.com>
2017-02-09 16:02:40 -0800

    default message: fast commit

--- Milestone 2 Part 2/RobotStates.c
+++ Milestone 2 Part 2/RobotStates.c
@@ -1,30 +1,32 @@
 #include "Robot.h"
 #include "Milestone2Claw.c"
 
-void resetAllEncoders() {
-			//resetMotorEncoder(MLeft);
-			//resetMotorEncoder(MRight);
-			resetMotorEncoder(MClaw);
+void setClaw(int i) {
+	motor[MClaw] = i;
 }
+
 // WHEN THE ROBOT IS IDLING, WAITING FOR BUTTON PRESSES
 T_state ProcessStateIdle(RobotControl & control) {
+	setClaw(0);
 	// BUTTON ONE TRANSITIONS TO OPENING
 	if (control.button1_pushed) {
 		control.button1_pushed = false;
-		resetAllEncoders();
 		return STATE_OPENING;
 	}
 	// BUTTON TWO TRANSITIONS TO MOVING
 	else if (control.button2_pushed) {
 			control.button2_pushed = false;
-			resetAllEncoders();
 			return STATE_CLOSING;
 	}
 	return STATE_IDLE;
 }
 T_state ProcessStateOpening(RobotControl & control) {
+	setClaw(10);
+	wait1Msec(1000);
 	return STATE_IDLE;
 }
 T_state ProcessStateClosing(RobotControl & control) {
+	setClaw(-10);
+	wait1Msec(1000);
 	return STATE_IDLE;
 }
